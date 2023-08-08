8712d4cbf44c93ce6db138801bbc402d7227dcdd
trunghaiii <haitran022001@gmail.com>
2023-08-08 19:41:12 -0400

    set up env config, mongoDB url, port and generate users module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,15 @@
 import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService } from "@nestjs/config"
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  //constructor(private readonly appService: AppService) {}
+  constructor(private configService: ConfigService) { }
 
   @Get()
-  @Render("home")
   processHomePage() {
-    let hometext = this.appService.getHello();
 
-    return {
-      text: hometext
-    }
+    return this.configService.get<string>('PORT')
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { ConfigService } from "@nestjs/config"
+import { UsersModule } from './users/users.module';
+
 
 @Module({
-  imports: [],
+  imports: [
+    // MongooseModule.forRoot('mongodb+srv://BookStore:g7X81S1QvQ5VovVY@cluster0.1grytvf.mongodb.net/nestDB'),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('DATABASE_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
