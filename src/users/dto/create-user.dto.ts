c7959082abff0e5baa78280a1d525cbb35f81a99
trunghaiii <haitran022001@gmail.com>
2023-08-09 22:55:25 -0400

    validate user data with class-validator

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,15 @@
+import {
+    IsNotEmpty,
+    IsEmail,
+} from 'class-validator';
+
 export class CreateUserDto {
+    @IsNotEmpty()
     name: string
+
+    @IsEmail()
+    @IsNotEmpty()
     email: string
+
     password: string
 }
